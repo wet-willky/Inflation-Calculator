e4ad2068c15d12e3ad6414c2186d353ed55ea732
wet-willky <wperreault@umassd.edu>
2025-12-04 09:04:18 -0500

    Final

--- main.c
+++ main.c
@@ -1,61 +1,62 @@
 #include<stdio.h>   
 
-int main(void) 
-{ 
-	int year1, year2; 
+int main(void)
+{
+	int year1, year2;
 	double amount1;
-	float inflation[25] = { .034,.028,.016,.023,.027,.034,.032,.028,.038,-.004, 
-    .016,.032,.021,.015,.016,.001,.013,.021,.024,.018,.012,.047,.08,.041,.029 }; 
+	float inflation[25] = { .034,.028,.016,.023,.027,.034,.032,.028,.038,-.004,
+	.016,.032,.021,.015,.016,.001,.013,.021,.024,.018,.012,.047,.08,.041,.029 };
 
-start: 
+start:
 
-	printf("Input The Starting Amount.\n"); 
-	scanf_s("%lf", &amount1); 
+	printf("Input The Starting Amount.\n");
+	scanf_s("%lf", &amount1);
 
 	//restarts the program if it is a negative amount input.
-	if (amount1 < 0) 
-	{ 
-		printf("error, negative number.\n\n\n", amount1); 
-		goto start; 
-	} 
-	printf("Input Year For Starting Amount.\n"); 
-	scanf_s("%d", &year1); 
+	if (amount1 < 0)
+	{
+		printf("error, negative number: %.2f\n\n\n", amount1);
+		goto start;
+	}
+
+	printf("Input Year For Starting Amount.\n");
+	scanf_s("%d", &year1);
 
 	//restarts program if year is less than 2000.  
-    if (year1 < 2000) 
-    { 
-		printf("Invalid Years Input Different Years.\n\n\n"); 
+	if (year1 < 2000)
+	{
+		printf("Invalid Years Input Different Years.\n\n\n");
 		goto start;
-	} 
+	}
 
-	printf("Input The Output Year\n"); 
-	scanf_s("%d", &year2); 
+	printf("Input The Output Year\n");
+	scanf_s("%d", &year2);
 
-    //restarts program if year is less than 2000.
-	if (year2 >= 2025) 
-    { 
-		printf("Invalid Years Input Different Years.\n\n\n"); 
-		goto start;  
-	} 
+	//restarts program if year is greater than 2025.
+	if (year2 >= 2025)
+	{
+		printf("Invalid Years Input Different Years.\n\n\n");
+		goto start;
+	}
 
-    //restarts program unless the years have are at least one year apart and non negative. 
-	if ( year2 - year1 < 0) 
-    { 
-		printf("Invalid Years Input Different Years.\n\n\n"); 
-		goto start; 
-	} 
-	else { 
-		int k = year1 - 2000; 
+	//restarts program unless the years have at least one year apart and non negative. 
+	if (year2 - year1 < 0)
+	{
+		printf("Invalid Years Input Different Years.\n\n\n");
+		goto start;
+	}
+	else {
+		int k = year1 - 2000;
 
-        //will repeat increasing the amount by the inflation amount until it reaches the year difference.  
-		for (int i = 1; i <= year2 - year1; i++) 
-        { 
-			amount1 = amount1 + amount1 * inflation[k]; 
-			k++; 
+		//will repeat increasing the amount by the inflation amount until it reaches the year difference.  
+		for (int i = 1; i <= year2 - year1; i++)
+		{
+			amount1 = amount1 + amount1 * inflation[k];
+			k++;
 		}
 
-		printf("The Value After Inflation Is: %.2lf\n", amount1); 
-        //restarts program after value is spat out. 
-		goto start; 
-	} 
-} 
+		printf("The Value After Inflation Is: %.2lf\n", amount1);
+		//restarts program after value is spat out. 
+		goto start;
+	}
+}
